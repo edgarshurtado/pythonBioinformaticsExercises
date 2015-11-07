8cdba85298eb9d9170731f9a290b689e48ca06c3
Edgar S. Hurtado <edsanhu@gmail.com>
2015-11-07 13:58:13 +0100

    Add more comments

--- TemarioWeb/listas/10.py
+++ TemarioWeb/listas/10.py
@@ -1,10 +1,12 @@
 '''
 File: 10.py
 Author: Me
-Description: Crea una lista con los números del 0 al 9 y extrae los pares, los impares, 
+Description: Crea una lista con los números del 0 al 9 y extrae los pares, los impares,
 los 5 primeros y los 5 últimos por separado.
 '''
 lista = list(range(10))
+    #range devuelve un iterable, por lo que hay que gastar el constructor de
+    #listas list() para guardarlo en una lista.
 
 #numeros pares
 print(lista[0::2])
@@ -14,4 +16,6 @@ print(lista[1::2])
 
 #5 primeros números
 print(lista[:5])
-print(lista[5:])
+
+#5 últimos números
+print(lista[-5:])
